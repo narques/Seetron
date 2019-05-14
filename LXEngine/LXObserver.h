7592c4ac980a165c017e71ee5e02149bbb581b72
Nicolas Arquès <nicolas.arques@gmail.com>
2019-05-13 23:58:44 -0400

    Deleted deprecated functions and classes.

--- LXEngine/LXObserver.h
+++ LXEngine/LXObserver.h
@@ -2,7 +2,7 @@
 //
 // This is a part of Seetron Engine
 //
-// Copyright (c) 2018 Nicolas Arques. All rights reserved.
+// Copyright (c) Nicolas Arques. All rights reserved.
 //
 //------------------------------------------------------------------------------------------------------
 
@@ -26,8 +26,6 @@ public:
 
 typedef list<LXObserver*> ListObservers;
 
-//------------------------------------------------------------------------
-
 class LXCORE_API LXObserverCommandManager : public LXObserver
 {
 
@@ -38,26 +36,9 @@ public:
 
 	virtual void OnFileSaved		( const LXSmartObject* pSmartObject ){};
 	virtual void OnChangeProperty	( const LXProperty* pProperty ){};
-	virtual void OnAddActors		( const SetActors& setActors ){};
-	virtual void OnDeleteActors		( const SetActors& setActors ){};
-	virtual void OnShowActors		( const SetActors& setActors ){};
 	virtual void OnChangeSelection	( const SetSmartObjects& setSmartObjects ){};
 	virtual void OnChangeHighlight	( LXActor* pActor ){};
-	virtual void OnDeleteMaterials	( const SetMaterials& setMaterial ){};
 	virtual void OnAddKeys			( const SetKeys& setKeys){};
 	virtual void OnDeleteKeys		( const SetKeys& setKeys){};
 	virtual void OnSceneChanged		( ){};
-};
-
-//------------------------------------------------------------------------
-
-class LXObserverScene : public LXObserver
-{
-
-public:
-
-	virtual void OnAddActors(const SetActors& setActors) {};
-	virtual void OnDeleteActors(const SetActors& setActors) {};
-	virtual void OnShowActors(const SetActors& setActors) {};
-	virtual void OnHideActors(const SetActors& setActors) {};
 };
