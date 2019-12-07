f6620b957311eb58b5ccbf1ea63dae44b08f223c
Nicolas Arquès <nicolas.arques@gmail.com>
2019-12-06 19:53:08 -0500

    Added win32 viewport windows

---
+++ LXEngine/LXWindowViewport.cpp
@@ -0,0 +1,129 @@
+//------------------------------------------------------------------------------------------------------
+//
+// This is a part of Seetron Engine
+//
+// Copyright (c) Nicolas Arques. All rights reserved.
+//
+//------------------------------------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "LXWindowViewport.h"
+#include "LXViewport.h"
+#include "LXRenderer.h"
+#include "LXMemory.h" // --- Must be the last included ---
+
+LXWindowViewport::LXWindowViewport(void* hWndParent)
+{
+	Create((HWND)hWndParent);
+
+	// Rendering
+	_viewport = new LXViewport();
+	_renderer = new LXRenderer(_hWND);
+	_renderer->SetViewport(_viewport);
+	RECT Rect;
+
+	GetClientRect(_hWND, &Rect);
+	_viewport->SetViewportSize(Rect.right - Rect.left, Rect.bottom - Rect.top);
+}
+
+LXWindowViewport::~LXWindowViewport()
+{
+
+}
+
+void LXWindowViewport::OnPaint()
+{
+	if (_viewport)
+		_viewport->OnPaint();
+}
+
+void LXWindowViewport::OnSize(int width, int height)
+{
+	if (_viewport)
+		_viewport->SetViewportSize(width, height);
+}
+
+void LXWindowViewport::OnLButtonDblClk(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnLButtonDblClk(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnLButtonDown(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnLButtonDown(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnLButtonUp(uint64 nFlags, LXPoint pntWnd)
+{
+	SetFocus(_hWND);
+	if (_viewport)
+		_viewport->OnLButtonUp(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnMButtonDown(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnMButtonDown(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnMButtonUp(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnMButtonUp(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnRButtonDown(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnRButtonDown(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnRButtonUp(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnRButtonUp(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnMouseWheel(uint16 nFlags, short zDelta, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnMouseWheel(nFlags, zDelta, pntWnd);
+}
+
+void LXWindowViewport::OnMouseMove(uint64 nFlags, LXPoint pntWnd)
+{
+	if (_viewport)
+		_viewport->OnMouseMove(nFlags, pntWnd);
+}
+
+void LXWindowViewport::OnKeyUp(uint64 KeyCode)
+{
+	if (_viewport)
+		_viewport->OnKeyUp(KeyCode, 0, 0);
+}
+
+void LXWindowViewport::OnKeyDown(uint64 KeyCode)
+{
+	if (_viewport)
+		_viewport->OnKeyDown(KeyCode, 0, 0);
+}
+
+bool LXWindowViewport::OnCommand(UINT CommandID)
+{
+	return false;
+}
+
+void LXWindowViewport::OnChar(uint64 CharacterCode)
+{
+	if (_viewport)
+		_viewport->OnChar(CharacterCode);
+}
+
+void LXWindowViewport::OnClose()
+{
+	LX_SAFE_DELETE(_renderer);
+	LX_SAFE_DELETE(_viewport);
+	LXWindow::OnClose();
+}
---
+++ LXEngine/LXWindowViewport.h
@@ -0,0 +1,54 @@
+//------------------------------------------------------------------------------------------------------
+//
+// This is a part of Seetron Engine
+//
+// Copyright (c) Nicolas Arques. All rights reserved.
+//
+//------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "LXWindow.h"
+
+class LXViewport;
+class LXRenderer;
+
+class LXCORE_API LXWindowViewport : public LXWindow
+{
+
+public:
+
+	LXWindowViewport(void* hwndParent);
+	virtual ~LXWindowViewport();
+
+	LXViewport* GetViewport() const { return _viewport; }
+	LXRenderer* GetRenderer() const { return _renderer; }
+
+public:
+
+	void OnClose() override;
+
+private:
+
+	bool OnCommand(UINT CommandID) override;
+	void OnChar(uint64 CharacterCode) override;
+	void OnKeyDown(uint64 KeyCode) override;
+	void OnKeyUp(uint64 KeyCode) override;
+	void OnLButtonDblClk(uint64 nFlags, LXPoint pntWnd) override;
+	void OnLButtonDown(uint64 nFlags, LXPoint pntWnd) override;
+	void OnLButtonUp(uint64 nFlags, LXPoint pntWnd) override;
+	void OnMButtonDown(uint64 nFlags, LXPoint pntWnd) override;
+	void OnMButtonUp(uint64 nFlags, LXPoint pntWnd) override;
+	void OnMouseMove(uint64 nFlags, LXPoint pntWnd) override;
+	void OnMouseWheel(uint16 nFlags, short zDelta, LXPoint pntWnd) override;
+	void OnPaint() override;
+	void OnRButtonDown(uint64 nFlags, LXPoint pntWnd) override;
+	void OnRButtonUp(uint64 nFlags, LXPoint pntWnd) override;
+	void OnSize(int width, int height) override;
+
+private:
+
+	LXViewport*  _viewport = nullptr;
+	LXRenderer* _renderer = nullptr;
+
+};
